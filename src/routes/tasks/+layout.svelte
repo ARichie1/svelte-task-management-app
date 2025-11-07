a956b7bf1428b0be98a4d854bbf23761d48305fe
Richard Akinmade <111028004+ARichie1@users.noreply.github.com>
2025-11-07 14:44:53 +0100

    updated  tasks page layout
    user can now add task on the /tasks route page

--- src/routes/tasks/+layout.svelte
+++ src/routes/tasks/+layout.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
-    import { tasks, taskStore } from '$lib/stores/taskStores';
-    import { onMount } from 'svelte';
-    import { get } from 'svelte/store';
+    import { tasks, completionPercentage } from '$lib/stores/taskStores';
     let { children } = $props()
+    import Modal from '$lib/components/shared/Modal.svelte';
+    import TaskForm from '$lib/components/TaskForm.svelte';
+
+    let showModal = $state(false)
+    const handleCancel = () => {showModal = false}
+    const openAddTask = () => {showModal = true;}
 </script>
 
 <div class="tasks-page-wrapper">
     <div class="tasks-page-header">
-        <h2>Your Available Tasks | <span>{$tasks.length}</span></h2>
+        <h4>Available Tasks | <span>{$tasks.length}</span></h4>
+        <div class="right-side">
+            <h4>Completed | {$completionPercentage}%</h4>
+            <button class="btn" onclick={openAddTask}>Add A New Task</button>
+        </div>
     </div>
     <div class="tasks-page-body scrollable">
         {@render children?.()}
     </div>
 </div>
+
+<!-- Modal Used To ADD Task To Any Quadrant -->
+<Modal open={showModal} onClose={handleCancel}>
+    <h3>Add A Task</h3>
+    <TaskForm onCancel={handleCancel}/>
+</Modal>
